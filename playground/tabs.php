4da7da295c6a3ac637cb731c788fe0dcc4f3ee8e
unknown <New Paltz@.newpaltz.edu>
2012-11-15 15:20:39 -0500

    looking into tabs

---
+++ playground/tabs.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>tabs</title>
+	<link rel="stylesheet" type="text/css" href="inc/tabstyles.css">
+	</head>
+
+<body>
+
+<div class="tabs">
+    
+   <div class="tab">
+       <input type="radio" id="tab-1" name="tab-group-1" checked>
+       <label for="tab-1">Tab One</label>
+       
+       <div class="content">
+           stuff1
+       </div> 
+   </div>
+    
+   <div class="tab">
+       <input type="radio" id="tab-2" name="tab-group-1">
+       <label for="tab-2">Tab Two</label>
+       
+       <div class="content">
+           stuff2
+       </div> 
+   </div>
+    
+    <div class="tab">
+       <input type="radio" id="tab-3" name="tab-group-1">
+       <label for="tab-3">Tab Three</label>
+     
+       <div class="content">
+           stuff3
+       </div> 
+   </div>
+    
+</div>
+
+
+</body>
+</html>
